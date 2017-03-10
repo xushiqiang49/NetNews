3a028241152b7b9edd4db2da74303aaad1af1dba
xushiqiang49 <xushiqiang49@126.com>
2017-03-10 21:31:43 +0800

    新闻视图的代理方法 新闻视图的滚动item设置

--- NetEase News/NetEase News/Classes/Home/ViewController/HomeViewController.m
+++ NetEase News/NetEase News/Classes/Home/ViewController/HomeViewController.m
@@ -11,7 +11,7 @@
 #import <YYModel.h>
 #import "TagLabel.h"
 
-@interface HomeViewController ()
+@interface HomeViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 
 @property(nonatomic, strong)NSArray<TagModel *> *tagModelArray;
 
@@ -20,6 +20,9 @@
 
 //新闻视图
 @property (weak, nonatomic) IBOutlet UICollectionView *newsCollectionView;
+
+//item的布局属性
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *NewCollectionViewFlowOut;
 @end
 
 @implementation HomeViewController
@@ -32,11 +35,59 @@
     //iOS 7 ,滚动视图向下默认偏移74
     self.automaticallyAdjustsScrollViewInsets = NO;
     
-    [self loadTagData];
+    [self setTageScrollView];
+    
+    [self setNewCollectionView];
 }
 
+#pragma mark -- 02 设置新闻滚动视图
+- (void)setNewCollectionView{
+    
+    //设置代理
+    self.newsCollectionView.dataSource = self;
+    
+    self.newsCollectionView.delegate = self;
+    
+    //设置item的大小
+    self.NewCollectionViewFlowOut.itemSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height - self.tagScrollView.frame.size.width - 64);
 
-- (void)loadTagData{
+    //设置垂直间距
+    self.NewCollectionViewFlowOut.minimumLineSpacing = 0;
+    
+    //设置水平间距
+    self.NewCollectionViewFlowOut.minimumInteritemSpacing = 0;
+    
+    //滚动方向
+    self.NewCollectionViewFlowOut.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    
+    //开启分页
+    self.newsCollectionView.pagingEnabled = YES;
+    
+    //去掉弹簧
+    self.newsCollectionView.bounces = NO;
+    
+    //去掉滚动条
+    self.newsCollectionView.showsHorizontalScrollIndicator = NO;
+    self.newsCollectionView.showsVerticalScrollIndicator = NO;
+    
+    //预加载
+    self.newsCollectionView.prefetchingEnabled = YES;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+
+    return self.tagModelArray.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"newsCell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark -- 01 设置频道标签视图
+- (void)setTageScrollView{
     
    self.tagModelArray = [TagModel getTagModelArray];
     
@@ -64,9 +115,9 @@
         
     }
     
-    
-    
 }
+
+
 
 
 @end
